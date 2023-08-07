9b6f51abcb5f686a37ff89a5af7162636f6a2975
Ayhamalahmad <amadae130@gmail.com>
2023-08-07 18:31:55 +0300

    handling buttons

--- javascript/index.js
+++ javascript/index.js
@@ -1,57 +1,68 @@
-// get Elements
-let prgress = document.querySelector(".container .prgress");
-let prgressSpan = document.querySelector(".pro-s");
-let buttonsContainer = document.querySelector(".buttons");
-let previousBtn = buttonsContainer.querySelector(".previous");
-let nextBtn = buttonsContainer.querySelector(".next");
-let active = document.querySelector(".active");
-let withPr = 0;
-let prgressWidth = 96;
-console.log(prgressWidth);
+let textout = document.querySelector(".text-out");
+let boldBtn = document.querySelector(".bold");
+let italicBtn = document.querySelector(".italic");
+let alignRightBtn = document.querySelector(".align-right");
+let alignLeftBtn = document.querySelector(".align-left");
+let alignCenterBtn = document.querySelector(".align-center");
+let sectionText = document.querySelector(".intro-text");
+let removeFormatTextBtn = document.querySelector(".reset");
+let selectAllBtn = document.querySelector(".selectAll");
+let underlineBtn = document.querySelector(".underline");
+let undoBtn = document.querySelector(".undo");
+let redoBtn = document.querySelector(".redo");
+let deleteBtn = document.querySelector(".delete");
+let copyBtn = document.querySelector(".copy");
+let H1Btn = document.querySelector(".H1");
+let H2Btn = document.querySelector(".H2");
+let H3Btn = document.querySelector(".H3");
 //
-buttonsContainer.addEventListener("click", (e) => {
-  if (e.target === previousBtn) {
-    deF();
-    switchToPrevious();
-  } else if (e.target === nextBtn) {
-    incrF();
-    switchToNext();
+function text(command, value) {
+  document.execCommand(command, false, value);
+}
+//
+function textAlign(textalign, value) {
+  document.execCommand("justify" + textalign, false, value);
+}
+function applyHeadingStyle(headingType) {
+  document.execCommand("formatBlock", false, headingType);
+}
+//
+function startListing(button, functionName) {
+  button.addEventListener("click", functionName);
+}
+startListing(boldBtn, () => text("bold", null));
+startListing(italicBtn, () => text("italic", null));
+startListing(removeFormatTextBtn, () => text("removeFormat", null));
+startListing(underlineBtn, () => text("underline", null));
+startListing(undoBtn, () => text("undo", null));
+startListing(redoBtn, () => text("redo", null));
+startListing(selectAllBtn, () => text("selectAll", null));
+startListing(deleteBtn, () => text("delete", null));
+startListing(copyBtn, () => text("copy", null));
+// applyHeadingStyle
+startListing(H1Btn, () => applyHeadingStyle("H1", null));
+startListing(H2Btn, () => applyHeadingStyle("H2", null));
+startListing(H3Btn, () => applyHeadingStyle("H3", null));
+// Text Aligns
+startListing(alignRightBtn, () => textAlign("Right", null));
+startListing(alignLeftBtn, () => textAlign("Left", null));
+startListing(alignCenterBtn, () => textAlign("Center", null));
+// Animate Text
+let i = 0;
+let milliseconds = 100;
+let editorTitle = "Welcome to Inspire: An Innovative Text Editor!";
+function startTextAnimation() {
+    let textInterval = setInterval(() => {
+      sectionText.textContent += editorTitle[i];
+      i++;
+      if (i === editorTitle.length) {
+        i = 0;
+        clearInterval(textInterval);
+        setTimeout(() => {
+          sectionText.textContent = "";
+          startTextAnimation();
+        }, 2000);
+      }
+    }, milliseconds);
   }
-});
-function incrF() {
-  if (withPr <= 96) {
-    withPr += 33;
-    prgressSpan.style.width = withPr + "%";
-  }
-}
-function deF() {
-  if (withPr !== 0) {
-    withPr -= 33;
-    prgressSpan.style.width = withPr + "%";
-  }
-}
-// functio
-let spans = document.querySelectorAll(".container .prgress span");
-let nextIndex = 0;
-let activeSpan = document.querySelector("span.active");
-let nextItem = document.querySelector("span:not(.active)");
-console.log(nextItem);
-
-function switchToNext() {
-  nextIndex = (nextIndex + 1) % spans.length;
-  console.log(`from switchToNext  ${nextIndex}`);
-  spans.forEach((span, index) => {
-    if (index === nextIndex) {
-      span.classList.add("active");
-    }
-  });
-}
-function switchToPrevious() {
-  nextIndex = (nextIndex - 1 + spans.length) % spans.length;
-  console.log(`from switchToPrevious  ${nextIndex}`);
-  spans.forEach((span, index) => {
-    if (index === nextIndex) {
-      span.classList.remove("active");
-    }
-  });
-}
+  startTextAnimation();
